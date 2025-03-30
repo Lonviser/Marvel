f49eb89eff694d32a2c631629eea4e3d8d112215
Ivan Lenkavets <47rex47@mail.ru>
2025-03-30 11:40:02 +0300

    feat: add service for work with api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
 import "./style/style.scss";
+import MarvelService from "./services/MarvelService";
+ 
+const marvelService = new MarvelService();
+
+
+
+// marvelService.getCharacter(32).then(res=>{console.log(res)});
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
