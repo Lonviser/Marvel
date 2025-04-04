6421ca0bde061045fe8d2a7125cc583ea01dd78c
Ivan Lenkavets <47rex47@mail.ru>
2025-04-04 20:07:45 +0300

    feat: add upload new cards on click button

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,65 +1,112 @@
-import './charList.scss';
-import { Component } from 'react';
+import {Component} from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
-
+import './charList.scss';
 
 class CharList extends Component {
-    state = {
-        charList: [],  // Массив вместо null
-        loading: true,
-        error: false
-    };
 
+    state = {
+        charList: [],
+        loading: true,
+        error: false,
+        newItemLoading: false,
+        offset: 210,
+        charEnded: false
+    }
+    
     marvelService = new MarvelService();
 
     componentDidMount() {
-        this.updateCharList();
+        this.onRequest();
     }
 
-    onCharListLoaded = (charList) => {
-        this.setState({ charList, loading: false });
-    };
-
-    onError = () => {
-        this.setState({ error: true, loading: false });
-    };
-
-    updateCharList = () => {
-        this.setState({ loading: true });
-        const offset = Math.floor(Math.random() * 500);  // Случайный offset от 0 до 4999
-    
+    onRequest = (offset) => {
+        this.onCharListLoading();
         this.marvelService.getAllCharacters(offset)
             .then(this.onCharListLoaded)
-            .catch(this.onError);
-    };
+            .catch(this.onError)
+    }
+
+    onCharListLoading = () => {
+        this.setState({
+            newItemLoading: true
+        })
+    }
+
+    onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
+        }
+
+        this.setState(({offset, charList}) => ({
+            charList: [...charList, ...newCharList],
+            loading: false,
+            newItemLoading: false,
+            offset: offset + 9,
+            charEnded: ended
+        }))
+    }
+
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    // Этот метод создан для оптимизации, 
+    // чтобы не помещать такую конструкцию в метод render
+    renderItems(arr) {
+        const items =  arr.map((item) => {
+            let imgStyle = {'objectFit' : 'cover'};
+            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = {'objectFit' : 'unset'};
+            }
+            
+            return (
+                <li 
+                    className="char__item"
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
+                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                        <div className="char__name">{item.name}</div>
+                </li>
+            )
+        });
+        // А эта конструкция вынесена для центровки спиннера/ошибки
+        return (
+            <ul className="char__grid">
+                {items}
+            </ul>
+        )
+    }
 
     render() {
-        const { charList, loading } = this.state;
+
+        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
         
+        const items = this.renderItems(charList);
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
+
         return (
             <div className="char__list">
-                <ul className="char__grid">
-                    {charList.map((char) => (
-                        <li 
-                        key={char.id}
-                        onClick={() => this.props.onCharSelected(char.id)}
-                        className="char__item">
-                            <img src={char.thumbnail} alt={char.name} style={char.thumbnail.includes('image_not_available') ? { objectFit: 'contain' } : null} />
-                            <div className="char__name">{char.name}</div>
-                        </li>
-                    ))}
-                </ul>
+                {errorMessage}
+                {spinner}
+                {content}
                 <button 
                     className="button button__main button__long"
-                    onClick={this.updateCharList}
-                    disabled={loading}
-                >
-                    <div className="inner">
-                        {loading ? "Loading..." : "Load more"}
-                    </div>
+                    disabled={newItemLoading}
+                    style={{'display': charEnded ? 'none' : 'block'}}
+                    onClick={() => this.onRequest(offset)}>
+                    <div className="inner">load more</div>
                 </button>
             </div>
-        );
+        )
     }
 }
 
